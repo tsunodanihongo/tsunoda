5d117e153cebc6ff6e77ebb0224dce16107d5fd7
Mina <miniki97@gmail.com>
2019-07-27 18:00:54 +0800

    update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,28 @@
-self.addEventListener('install', function(event) {
+'use strict';
+
+// CODELAB: Update cache names any time any of the cached files change.
+const CACHE_NAME = 'static-cache-v1';
+
+// CODELAB: Add list of files to cache here.
+const FILES_TO_CACHE = [
+];
+
+self.addEventListener('install', (evt) => {
+  console.log('[ServiceWorker] Install');
+  // CODELAB: Precache static resources here.
+
   self.skipWaiting();
-  
-  var offlinePage = new Request('offline.html');
-  event.waitUntil(
-  fetch(offlinePage).then(function(response) {
-    return caches.open('offline2').then(function(cache) {
-      return cache.put(offlinePage, response);
-    });
-  }));
-}); self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function(error) {
-        return caches.open('offline2').then(function(cache) {
-          return cache.match('offline.html');
-      });
-    }));
-}); self.addEventListener('refreshOffline', function(response) {
-  return caches.open('offline2').then(function(cache) {
-    return cache.put(offlinePage, response);
-  });
-}); self.addEventListener('push', function (event) {
-  var data = event.data.json();   var opts = {
-    body: data.body,
-    icon: data.icon,
-    data: {
-      url: data.url
-    }
-  };
-  event.waitUntil(self.registration.showNotification(data.title, opts));
-}); self.addEventListener('notificationclick', function(event) {
-  var data = event.notification.data;   event.notification.close();   event.waitUntil(
-    clients.openWindow(data.url)
-  );
+});
+
+self.addEventListener('activate', (evt) => {
+  console.log('[ServiceWorker] Activate');
+  // CODELAB: Remove previous cached data from disk.
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  console.log('[ServiceWorker] Fetch', evt.request.url);
+  // CODELAB: Add fetch event handler here.
+
 });
